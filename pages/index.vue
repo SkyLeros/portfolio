0e240428ed784cea052de90230bae30e0b6b75a1
SkyLeros <yehongt@gmail.com>
2024-08-02 00:59:56 +0800

    refactor: Update background color in homepage
    Update background color in homepage and AppNavbar.vue to #1e293b for improved visual appearance and readability.

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,9 @@ import { Icon } from "@iconify/vue";
 <template>
   <div
     v-motion-fade-visible-once
-    class="flex flex-col justify-center py-12 md:mx-4 xl:px-0"
+    class="bg-[#1e293b] flex flex-col justify-center py-12 md:mx-4 xl:px-0"
   >
-    <div class="container mx-auto h-full bg-[#1e293b]">
+    <div class="container mx-auto h-full">
       <div
         class="flex flex-col items-center justify-between xl:flex-row xl:gap-6"
       >
