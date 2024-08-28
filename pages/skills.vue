e88c4ebdec61ae8cc4743fd95064f43d86b2f9f0
SkyLeros <yehongt@gmail.com>
2024-08-28 11:42:03 +0800

    refactor: Rearrange order of skills in skills.vue
    Rearrange the order of skills in the skills.vue file to match the order of importance and relevance. This improves the user experience by showcasing the most important skills first and aligning with industry standards.

--- pages/skills.vue
+++ pages/skills.vue
@@ -11,7 +11,7 @@ import { Icon } from "@iconify/vue";
 <template>
   <div v-motion-fade class="container mx-auto">
     <div class="mb-5 gap-4 text-center">
-      <h1 class="text-4xl lg:text-5xl font-extrabold text-blue-500">Skills</h1>
+      <h1 class="text-4xl font-extrabold text-blue-500 lg:text-5xl">Skills</h1>
     </div>
     <div
       class="mx-4 grid grid-cols-1 gap-6 p-4 text-white sm:mx-6 sm:grid-cols-2 md:mx-10 md:grid-cols-3 lg:mx-20 lg:gap-6"
@@ -41,6 +41,22 @@ import { Icon } from "@iconify/vue";
             class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
           >
             <Icon
+              icon="akar-icons:typescript-fill"
+              class="mx-auto h-16 w-16 md:h-24 md:w-24"
+            />
+            <span class="mt-2 block text-center font-bold">Typescript</span>
+          </TooltipTrigger>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider
+        class="flex items-center justify-center"
+        :delayDuration="100"
+      >
+        <Tooltip>
+          <TooltipTrigger
+            class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
+          >
+            <Icon
               icon="fa6-brands:vuejs"
               class="mx-auto h-16 w-16 md:h-24 md:w-24"
             />
@@ -48,6 +64,8 @@ import { Icon } from "@iconify/vue";
           </TooltipTrigger>
         </Tooltip>
       </TooltipProvider>
+
+      <!-- Second Row -->
       <TooltipProvider
         class="flex items-center justify-center"
         :delayDuration="110"
@@ -61,24 +79,6 @@ import { Icon } from "@iconify/vue";
               class="mx-auto h-16 w-16 md:h-24 md:w-24"
             />
             <span class="mt-2 block text-center font-bold">Nuxt Js</span>
-          </TooltipTrigger>
-        </Tooltip>
-      </TooltipProvider>
-
-      <!-- Second Row -->
-      <TooltipProvider
-        class="flex items-center justify-center"
-        :delayDuration="100"
-      >
-        <Tooltip>
-          <TooltipTrigger
-            class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
-          >
-            <Icon
-              icon="ri:tailwind-css-fill"
-              class="mx-auto h-16 w-16 md:h-24 md:w-24"
-            />
-            <span class="mt-2 block text-center font-bold">Tailwind CSS</span>
           </TooltipTrigger>
         </Tooltip>
       </TooltipProvider>
@@ -125,6 +125,22 @@ import { Icon } from "@iconify/vue";
             class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
           >
             <Icon
+              icon="ri:tailwind-css-fill"
+              class="mx-auto h-16 w-16 md:h-24 md:w-24"
+            />
+            <span class="mt-2 block text-center font-bold">Tailwind CSS</span>
+          </TooltipTrigger>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider
+        class="flex items-center justify-center"
+        :delayDuration="100"
+      >
+        <Tooltip>
+          <TooltipTrigger
+            class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
+          >
+            <Icon
               icon="devicon-plain:mysql-wordmark"
               class="mx-auto h-16 w-16 md:h-24 md:w-24"
             />
@@ -140,8 +156,39 @@ import { Icon } from "@iconify/vue";
           <TooltipTrigger
             class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
           >
+            <Icon
+              icon="simple-icons:sqlite"
+              class="mx-auto h-16 w-16 md:h-24 md:w-24"
+            />
+            <span class="mt-2 block text-center font-bold">SQLite</span>
+          </TooltipTrigger>
+        </Tooltip>
+      </TooltipProvider>
+
+      <!-- Fourth row -->
+      <TooltipProvider
+        class="flex items-center justify-center"
+        :delayDuration="100"
+      >
+        <Tooltip>
+          <TooltipTrigger
+            class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
+          >
             <Icon icon="bi:git" class="mx-auto h-16 w-16 md:h-24 md:w-24" />
             <span class="mt-2 block text-center font-bold">Git</span>
+          </TooltipTrigger>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider
+        class="flex items-center justify-center"
+        :delayDuration="100"
+      >
+        <Tooltip>
+          <TooltipTrigger
+            class="rounded-md border-2 border-gray-500 p-4 hover:border-blue-500 hover:bg-slate-700"
+          >
+            <Icon icon="uil:docker" class="mx-auto h-16 w-16 md:h-24 md:w-24" />
+            <span class="mt-2 block text-center font-bold">Docker</span>
           </TooltipTrigger>
         </Tooltip>
       </TooltipProvider>
